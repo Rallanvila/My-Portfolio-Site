89e7070a15ebeae68419111e08ff2122b4a546d2
Allan <74523880+Rallanvila@users.noreply.github.com>
2021-03-03 01:25:49 -0500

    added what i do ps

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
 gsap.to(".headline", {duration: 4, x: 300});
-gsap.from(".about", {duration: 3, x: 400})
-
-gsap.to('.headline', {duration: 3 , x: 200})
-
-gsap.to('img', {duration: 3, x: 300})
+gsap.to(".logo", {duration: 2, x: 200});
