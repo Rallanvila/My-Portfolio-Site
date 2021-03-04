873ee7a8743297300373990f2641ff7dbbe0847a
Allan <74523880+Rallanvila@users.noreply.github.com>
2021-03-04 00:12:56 -0500

    fixed vh and added gsap

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
-gsap.to(".headline", {duration: 4, x: 300});
-gsap.to(".logo", {duration: 2, x: 200});
+gsap.to(".headline", {
+    duration: 3, 
+    x: 200,
+})
+
+gsap.from(".headline", {
+    opacity: 0, 
+    duration: 4
+})
+
+gsap.from(".nav", {
+    duration: 8, 
+    opacity: 0, 
+})
+
+gsap.from(".about", {
+    opacity: 0,
+    duration: 1.5,
+    scrollTrigger: {
+        trigger: ".about",
+        start: "top center"
+    }
+})
+
+gsap.from(".do-img", {
+    scrollTrigger: {
+        trigger: ".do-img",
+        start: "top center"
+    },
+    duration: 2,
+    opacity: 0,
+    stagger: .5,
+})
