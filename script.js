f893a4ab995ae536e294208f0f7f34746fa7f79a
Allan <74523880+Rallanvila@users.noreply.github.com>
2021-03-07 14:38:53 -0500

    fixed projeccts media query

--- script.js
+++ script.js
@@ -68,3 +68,46 @@ gsap.from(".fab", {
     y: -150,
     ease: "back"
 })
+
+gsap.from(".what-ive-done", {
+    opacity: 0,
+    duration: 1.5,
+    scrollTrigger: {
+        trigger: ".what-ive-done",
+        start: "top center"
+    }
+})
+
+// ----------popup-----------
+
+const openPopupButtons = document.querySelectorAll(`[data-popup-target]`);
+
+const closePopupButtons = document.querySelectorAll(`[data-close-button]`);
+
+const overlay= document.getElementById(`overlay`);
+
+openPopupButtons.forEach(button=> {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.popupTarget)
+        openPopup(popup)
+    })
+})
+
+closePopupButtons.forEach(button=> {
+    button.addEventListener('click', () => {
+        const popup = button.closest('.popup')
+        closePopup(popup)
+    })
+})
+
+function openPopup(popup) {
+    if (modall == null) return
+    popup.classList.add('active')
+    popup-overlay.classList.add('active')
+}
+
+function closePopup(popup) {
+    if (modall == null) return
+    popup.classList.remove('active')
+    popup-overlay.classList.remove('active')
+}
